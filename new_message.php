3f380a6966eb36f830b25bb4e23b3b184ea7e3f5
federico <federico.crivellaro@sine.com>
2016-10-28 13:54:07 +0200

    test

--- new_message.php
+++ new_message.php
@@ -2,6 +2,24 @@
 
 header('Content-Type: application/json');
 
+
+
+  function prompt($prompt_msg){
+      echo("<script type='text/javascript'> var answer = prompt('".$prompt_msg."'); </script>");
+
+      $answer = "<script type='text/javascript'> document.write(answer); </script>";
+      return($answer);
+  }
+
+   //program
+  $prompt_msg = "Please type your name.";
+  $name = prompt($prompt_msg);
+
+  $output_msg = "Hello there ".$name."!";
+  echo($output_msg);
+  
+
+
 $errors= "";
 $message = (object) $_POST;
 $response = array();
